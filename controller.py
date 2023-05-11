2bf186acf58df60613e1e0e63310238edb0715fb
zkaki <zkakish@gmail.com>
2023-05-10 20:38:32 -0500

    added new functions

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
 from view import *
+from math import *
 
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
@@ -30,6 +31,14 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.divide_button.clicked.connect(lambda: self.divide())
         self.floored_button.clicked.connect(lambda: self.floored())
         self.modulo_button.clicked.connect(lambda: self.modulo())
+        self.x_squared_button.clicked.connect(lambda: self.square())
+        self.x_cubed_button.clicked.connect(lambda: self.cube())
+        self.square_root_button.clicked.connect(lambda: self.root())
+        self.cube_root_button.clicked.connect(lambda: self.cuberoot())
+        self.sin_button.clicked.connect(lambda: self.sine())
+        self.cos_button.clicked.connect(lambda: self.cosine())
+        self.backspace_button.clicked.connect(lambda: self.backspace())
+
 
     def one(self):
         if len(Controller.Num) >= 1 and Controller.Sign == '':
@@ -168,12 +177,19 @@ class Controller(QMainWindow, Ui_MainWindow):
                 self.answer_text.setText(f'{Controller.Num} {Controller.Sign} {Controller.Num2}')
 
     def zero(self):
-        if Controller.Num == 0:
-            Controller.Num = 0
+        if len(Controller.Num) >= 1 and Controller.Sign == '':
+            Controller.Num += '0'
+            self.answer_text.setText(f'{Controller.Num}')
+        elif Controller.Num == '':
+            Controller.Num = '0'
             self.answer_text.setText(f'{Controller.Num}')
         else:
-            Controller.Num2 = 0
-            self.answer_text.setText(f'{Controller.Num} {Controller.Sign} {Controller.Num2}')
+            if len(Controller.Num2) >= 1:
+                Controller.Num2 += '0'
+                self.answer_text.setText(f'{Controller.Num} {Controller.Sign} {Controller.Num2}')
+            else:
+                Controller.Num2 = '0'
+                self.answer_text.setText(f'{Controller.Num} {Controller.Sign} {Controller.Num2}')
 
 
 
@@ -228,7 +244,62 @@ class Controller(QMainWindow, Ui_MainWindow):
             Controller.Num = str(Controller.Total)
             self.answer_text.setText(f'{Controller.Num} %')
 
+    def square(self):
+        if Controller.Total == 0:
+            Controller.Sign = '^2'
+            self.answer_text.setText(f'{Controller.Num} ^2')
+        else:
+            Controller.Num = str(Controller.Total)
+            self.answer_text.setText(f'{Controller.Num} ^2')
 
+    def cube(self):
+        if Controller.Total == 0:
+            Controller.Sign = '^3'
+            self.answer_text.setText(f'{Controller.Num} ^3')
+        else:
+            Controller.Num = str(Controller.Total)
+            self.answer_text.setText(f'{Controller.Num} ^3')
+
+
+    def root(self):
+        if Controller.Total == 0:
+            Controller.Sign = 'r'
+            self.answer_text.setText(f'{Controller.Num} r')
+        else:
+            Controller.Num = str(Controller.Total)
+            self.answer_text.setText(f'{Controller.Num} r')
+
+    def cuberoot(self):
+        if Controller.Total == 0:
+            Controller.Sign = 'c'
+            self.answer_text.setText(f'{Controller.Num} c')
+        else:
+            Controller.Num = str(Controller.Total)
+            self.answer_text.setText(f'{Controller.Num} c')
+
+
+    def sine(self):
+        if Controller.Total == 0:
+            Controller.Sign = 'sin'
+            self.answer_text.setText(f'sin({Controller.Num})')
+        else:
+            Controller.Num = str(Controller.Total)
+            self.answer_text.setText(f'sin({Controller.Num})')
+
+    def cosine(self):
+        if Controller.Total == 0:
+            Controller.Sign = 'cosine'
+            self.answer_text.setText(f'cos({Controller.Num})')
+        else:
+            Controller.Num = str(Controller.Total)
+            self.answer_text.setText(f'cos({Controller.Num})')
+    def backspace(self):
+        if Controller.Sign == '':
+            Controller.Num = Controller.Num[:-1]
+            self.answer_text.setText(f'{Controller.Num}')
+        else:
+            Controller.Num2 = Controller.Num2[:-1]
+            self.answer_text.setText(f'{Controller.Num} {Controller.Sign} {Controller.Num2}')
 
 
 
@@ -246,6 +317,18 @@ class Controller(QMainWindow, Ui_MainWindow):
             Controller.Total = int(Controller.Num) // int(Controller.Num2)
         elif Controller.Sign == '%':
             Controller.Total = int(Controller.Num) % int(Controller.Num2)
+        elif Controller.Sign == '^2':
+            Controller.Total = int(Controller.Num) ** 2
+        elif Controller.Sign == '^3':
+            Controller.Total = int(Controller.Num) ** 3
+        elif Controller.Sign == 'r':
+            Controller.Total = sqrt(int(Controller.Num))
+        elif Controller.Sign == 'c':
+            Controller.Total = int(Controller.Num) ** (1/3)
+        elif Controller.Sign == 'sin':
+            Controller.Total = sin(int(Controller.Num))
+        elif Controller.Sign == 'sin':
+            Controller.Total = cos(int(Controller.Num))
         self.answer_text.setText(f'{Controller.Total}')
         Controller.Num2 = ''
 
